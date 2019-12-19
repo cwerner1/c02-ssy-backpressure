c6ff02751a10b0ef5123dc26f7fd0b11ee26c7f6
Arno Hollosi <ahollosi@xmp.net>
2019-12-19 11:09:03 +0100

    Summen-Service mit Worker (frägt Queue ab) implementiert

--- routes/sum.js
+++ routes/sum.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const db = require('../src/database');
+/*
+const db = {
+    database: db,
+    sumCollection: sum
+};
+ */
 const Request = require('request');
+const config = require('../src/config');
 const router = express.Router();
 
 
@@ -8,11 +15,48 @@ router.get('/:id', getItem);
 router.get('/', getAll);
 
 function getItem(req, res) {
-
+    /* { id: ...., sum: .... } */
+    let item = db.sumCollection.findOne({id: parseInt(req.params.id) });
+    res.json(item);
 }
 
 function getAll(req, res) {
+    res.json(db.sumCollection.find());
+}
 
+setTimeout(pollQueue, config.WORKER_TIMEOUT);
+
+function pollQueue() {
+    setTimeout(pollQueue, config.WORKER_TIMEOUT);
+    Request.get({
+        url: 'http://127.0.0.1:3000/queue',
+        json: true
+    }, queueResponse);
+
+    function queueResponse(err, resp, body) {
+        if (resp.statusCode === 204) {
+            return;
+        }
+
+        /* body:
+        {
+            id: util.randomId(10),
+            number: number
+        }
+         */
+
+        let item = db.sumCollection.findOne({id: body.id});
+        if (item === null) {
+            /* DB: { id: ...., sum: .... } */
+            db.sumCollection.insert({
+                id: body.id,
+                sum: body.number
+            });
+        } else {
+            item.sum += body.number;
+            db.sumCollection.update(item);
+        }
+    }
 }
 
 
